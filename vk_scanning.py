cb7ccd768476d98ff78af09ac3effc38674b7172
denisxab <47678176+denisxab@users.noreply.github.com>
2019-07-12 01:04:19 +0300

    Add files via upload

--- vk_scanning.py
+++ vk_scanning.py
@@ -1,73 +1,86 @@
-import os
+import requests
+import threading
 import json
-import time
-from selenium import webdriver 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import Firefox 
-from selenium.webdriver.firefox.options import Options
+import os
+
+#------------------------------------------------------#
+def cleaning_id(all_id,group):
+	n = []
+	for x in all_id:
+		if x[1] == 1:
+			if x[2] >= 1999 and x[2] <= 2001:
+				if x[3] == 2:
+					if x[4] == 1:
+						if x[5] <= 800:
+							if x[6] == 0 or x[6] == 6 or x[6] == 1:
+								n.append(x)
+
+	with open('{}\\ID_gerl.json'.format(group),'w') as file_json:
+		json.dump(n,file_json)
+########################################################
+def vK_skan(отступ,group,token,v):
+	global all_id
+	counts=1000
+	offset = отступ[0]
+	while offset <= отступ[1]:
+		respons = requests.get('https://api.vk.com/method/groups.getMembers', params={
+				"access_token":token,
+				'v':v,
+				"group_id":group,
+				'count':counts,
+				'offset':offset,
+				'fields':'sex, city, bdate,followers_count,relation,can_write_private_message'
+				})
+		offset+=counts
+		try:
+			for i in respons.json()['response']['items']:
+				try:
+					a = (i['id'],i['sex'],int(i['bdate'].split('.')[2]),i['city']['id'] ,i['can_write_private_message'] ,i['followers_count'] ,i['relation'])
+					all_id.append(a)
+				except:
+					continue
+		except KeyError:
+			print(respons.json())
+########################################################
+def sorting_numbers(number_all,stream):
+	b = number_all//(stream) # 10
+	v = []
+	d = []
+	for u in range(stream):
+		v.append(number_all)
+		number_all = number_all - b
+		if u > 0:
+			f = v[u],v[u-1]
+			d.append(f)
+			if u+1 == stream:
+				v.append(number_all)
+				f = v[u+1],v[u]
+				d.append(f)
+	return d
+#------------------------------------------------------#
+
+
+a = 908500
+stream = 30
+
+
+os.system('Name.txt')
+with open('Name.txt','r') as File_name:
+	group = File_name.read()
 
 
 
-def openvk (b):
-	try:
-		b.get('https://vk.com/im')
-		t=b.find_element_by_id('email')
-		t.send_keys('')
-		t=b.find_element_by_id('pass')
-		t.send_keys('')
-		s=b.find_element_by_id('login_button')
-		s.click()
-		time.sleep(1)
-	except NameError :
-		sf = b.find_element_by_name("box_x_button")
-		sf.click()
-
-
-
-
-
-
-
-b=webdriver.Firefox()
-openvk(b)
-
-
-u = 0
-for x in os.listdir():
-	if x == 'ID_gerl1.json':
-		with open('ID_gerl1.json','r') as file_json:
-			vk_clr = json.load(file_json)
-		u = 1
-		break
-if u == 0:
-	with open('ID_gerl.json','r') as file_json:
-		vk_clr = []
-		for x in json.load(file_json):
-			vk_clr.append(x[0])
-		with open('ID_gerl1.json','w') as file_json:
-			json.dump(vk_clr,file_json)
-
-
-
-
-
-try:
-	for x in range(len(vk_clr)):
-		b.get('https://vk.com/id{}'.format(vk_clr[x]))
-		print('------------------------')
-		resl = input("Что делаем ? {} - {}".format(len(vk_clr),vk_clr[x]))
-		if resl == '+':
-			with open('Like.txt','a') as file_txt:
-				file_txt.write('|{}|'.format(vk_clr[x]))
-		if resl == 'S':
-			for yuy in range(x):
-				vk_clr.pop(0)
-			with open('ID_gerl1.json','w') as file_json:
-				json.dump(vk_clr,file_json)
-				break
-
-
-finally:
-	with open('ID_gerl1.json','w') as file_json:
-		json.dump(vk_clr,file_json)
-
+os.mkdir(group) 
+token = "f289aa40f289aa40f289aa4039f2e03297ff289f289aa40ae2c6b5129292562a1d4c02f"
+v = "5.74"
+result_function = sorting_numbers(a,stream)
+all_id = []
+threading_list=[]
+for x in range(stream):
+	t = threading.Thread(target=vK_skan,args=(result_function[x],group,token,v))
+	threading_list.append(t)
+	t.start()
+for y in threading_list:
+	y.join()
+cleaning_id(all_id,group)
+print('++++++++++++++++++++++++++')
